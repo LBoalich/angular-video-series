ea6bde63a1213eecce43ce6f621be88eff47494f
LBoalich <lkboalich@gmail.com>
2023-08-24 12:08:28 -0400

    Use ViewChild to send Message from child to parent

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent {
+export class ParentComponent implements OnInit {
+  @ViewChild(ChildComponent) child;
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  receiveMessage(msg) {
+    alert(msg);
+  }
+
+  ngAfterViewInit() {
+    alert(this.child.Message);
+  }
 }
